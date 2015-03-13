db525c12fdf93ed89e71dd6c4386e1e72a48ffc3
root <root@laptoprafal.(none)>
2015-03-13 21:07:35 +0100

    Added SearchActivity and connected search toolbar button from MainActivity to it

--- app/src/main/java/com/example/root/myapplication/MainActivity.java
+++ app/src/main/java/com/example/root/myapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.root.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -40,17 +41,14 @@ public class MainActivity extends ActionBarActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            case R.id.action_search:
+                startActivity(new Intent(getApplicationContext(), SearchActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-
-        return super.onOptionsItemSelected(item);
     }
 
 }
