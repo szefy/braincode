3cf2e72771e0bfd55f0424a7de916ac04511061e
Rafal Michalek <rafmichalek@gmail.com>
2015-03-14 16:57:20 +0100

    Noti fix2

--- app/src/main/java/com/example/root/myapplication/StreamStatusService.java
+++ app/src/main/java/com/example/root/myapplication/StreamStatusService.java
@@ -82,6 +82,7 @@ public class StreamStatusService extends Service{
         Notification noti = new Notification.Builder(getApplicationContext())
                 .setContentTitle(channelTitle + " started streaming")
                 .setContentText(channelTitle + " started streaming")
+                .setSmallIcon(R.drawable.icon_search)
                 .build();
         NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
 
